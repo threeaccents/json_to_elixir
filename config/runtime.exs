71140c8305d1af9b02b06590a37137336a588e19
threeaccents <rodrigo@threeaccents.com>
2023-06-26 10:55:46 -0600

    update host

--- config/runtime.exs
+++ config/runtime.exs
@@ -33,8 +33,8 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
-  port = String.to_integer(System.get_env("PORT") || "4000")
+  host = System.get_env("RENDER_EXTERNAL_HOSTNAME") || "jsontoelixir.com"
+  port = String.to_integer(System.get_env("PORT") || "80")
 
   config :json_to_elixir, JTEWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
