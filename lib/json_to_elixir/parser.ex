0f88031984dff04c5f8c82154188e17c17183c2e
Rodrigo Lessa <rodrigo@threeaccents.com>
2023-06-27 08:37:43 -0600

    Ta/proper elixir types (#10)
    * transform null to string
    
    * update bool to boolean

--- lib/json_to_elixir/parser.ex
+++ lib/json_to_elixir/parser.ex
@@ -1,6 +1,6 @@
 defmodule JTE.Parser do
   @moduledoc """
-  JSON parser. 
+  JSON parser.
   It expects the tokens generarted by the lexer.
   Since the lexer only takes in valid JSON the parser
   can expect to receive only valid tokens.
@@ -167,5 +167,6 @@ defmodule JTE.Parser do
   defp atom(str), do: ":string_to_atom__#{str}"
 
   defp schema_type(:null), do: :string
+  defp schema_type(:bool), do: :boolean
   defp schema_type(type), do: type
 end
